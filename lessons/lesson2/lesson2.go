58e68e051b5b076bbc611b23117c4575d0e4542b
blogic-hoanglongnguyen <hoanglongnguyen@blogiclab.com>
2026-02-11 11:22:14 +0700

    done lesson 2

--- lessons/lesson2/lesson2.go
+++ lessons/lesson2/lesson2.go
@@ -2,7 +2,73 @@ package lesson2
 
 import "fmt"
 
+type User struct {
+	ID int
+	Name string
+	Age int
+}
+
+type Student struct {
+	User
+	Class string
+	Score int
+}
+
 func Run(i int) {
 	var name string = "Long Nguyen"
 	fmt.Println("Hello", name, "index", i)
+}
+
+func ReplaceSliceIndex() {
+	//Add slice 
+	a := []int{1, 2, 3}
+	fmt.Println("Before replace first index safe", a)
+	b := ReplaceFirstIndexSafe(a, 4)
+	fmt.Println("After replace first index safe", a)
+	fmt.Println("After replace first index safe", b)
+}
+
+func ReplaceFirstIndex(slice []int, value int) []int {
+	slice[0] = value
+	return slice
+}
+
+func ReplaceFirstIndexSafe(slice []int, value int) []int {
+    copied := make([]int, len(slice))
+    copy(copied, slice)
+
+    copied[0] = value
+    return copied
+}
+
+func PrintUserInfo(user User) {
+	fmt.Println("User name:", user.Name)
+	fmt.Println("User age:", user.Age)
+}
+
+func PrintEventNumber(numbers []int) {
+	for _, number := range numbers {
+		if number % 2 == 0 {
+			fmt.Println(number)
+		}
+	}
+}
+
+func PrintStudentRankAndInfo(student Student) {
+	fmt.Println("Student ID:", student.User.ID)
+	fmt.Println("Student Name:", student.User.Name)
+	fmt.Println("Student Age:", student.User.Age)
+	fmt.Println("Student Class:", student.Class)
+	fmt.Println("Student Score:", GetStudentRank(student.Score))
+}
+
+func GetStudentRank(score int) string {
+	switch {
+		case score >= 9: 
+			return "A"
+		case score >= 8:
+			return "B"
+		default:
+			return "c"
+	}
 }
--- main.go
+++ main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
-	// "learn-golang/lessons/lesson1"
 	"fmt"
 	"learn-golang/lessons/lesson2"
 )
 
 func main() {
 	// lesson1.Run()
-	var a [3]int = [3]int{1, 2, 3}
-	for i := 0; i < len(a); i++ {
-		lesson2.Run(i)
+	// lesson2.Run(i)
+	// lesson2.ReplaceSliceIndex()
+
+	// users := []lesson2.User{
+	// 	{Name: "Long Nguyen", Age: 20, Email: "longnguyen@gmail.com"},
+	// 	{Name: "Long Nguyen", Age: 20, Email: "longnguyen@gmail.com"},
+	// }
+
+	// for index := 0; index < len(users); index++ {
+	// 	lesson2.PrintUserInfo(users[index])
+	// 	fmt.Println("--------------------------------")
+	// }
+
+	// eventNumbers := []int{}
+	// for i := 0; i < 30; i++ {
+	// 	eventNumbers = append(eventNumbers, i+1)
+	// }
+	// lesson2.PrintEventNumber(eventNumbers)
+
+	student := []lesson2.Student{
+		{
+			User:  lesson2.User{ID: 1, Name: "Long Nguyen", Age: 20},
+			Class: "A",
+			Score: 9,
+		},
+		{
+			User:  lesson2.User{ID: 2, Name: "Toan Thai", Age: 30},
+			Class: "B",
+			Score: 6,
+		},
+		{
+			User:  lesson2.User{ID: 3, Name: "Tu Nguyen", Age: 20},
+			Class: "C",
+			Score: 8,
+		},
 	}
-	fmt.Println("Press Enter to exit...")
-	fmt.Scanln()
+
+	for _, student := range student {
+		lesson2.PrintStudentRankAndInfo(student)
+		fmt.Println("--------------------------------")
+	}
 }
